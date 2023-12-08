a0e484d7e25ae1e9e2350005b5593b4775c1e3cd
hasundue <hasundue@gmail.com>
2023-12-08 09:05:49 +0900

    do not generate plugins/flake.nix

--- build.ts
+++ build.ts
@@ -1,22 +1,20 @@
 import { assertInstanceOf } from "https://deno.land/std@0.208.0/assert/assert_instance_of.ts";
 import { $HOME, type ClosedGroup } from "./rc/dpp/helper/mod.ts";
 
-const HEAD = `{
-  description = "hasundue's Neovim plugins (auto-generated)";
-
-  inputs = {`;
-
-const TAIL = `  };
-
-  outputs = inputs: {
-    plugins = inputs;
-  };
-}
-`;
-
-async function generateFlake(
+async function updateFlake(
   plugins: ClosedGroup,
 ) {
+  const original = await Deno.readTextFile(
+    new URL("./flake.nix", import.meta.url),
+  );
+  const lines = original.split("\n");
+  const head = lines.slice(
+    0,
+    lines.indexOf("    /* PLUGINS START */\n") + 1,
+  );
+  const tail = lines.slice(
+    lines.indexOf("    /* PLUGINS END */"),
+  );
   try {
     await Deno.mkdir(new URL("./plugins", import.meta.url), {
       recursive: true,
@@ -24,20 +22,20 @@ async function generateFlake(
   } catch (e) {
     assertInstanceOf(e, Deno.errors.AlreadyExists);
   }
-  const lines = plugins.map((it) => {
+  const insertion = plugins.map((it) => {
     const { name, repo } = it;
     const url = repo.startsWith("~")
       ? `git+file:${repo.replace("~", $HOME)}`
       : `github:${repo}`;
-    return `    "${name}" = { url = "${url}"; flake = false; };`;
+    return `    "plugin:${name}" = { url = "${url}"; flake = false; };`;
   });
   await Deno.writeTextFile(
     new URL("./plugins/flake.nix", import.meta.url),
-    [HEAD, lines, TAIL].flat().join("\n"),
+    [head, insertion, tail].flat().join("\n"),
   );
 }
 
 if (import.meta.main) {
   const { PLUGINS } = await import("./rc/dpp/plugins.ts");
-  await generateFlake(PLUGINS);
+  await updateFlake(PLUGINS);
 }
