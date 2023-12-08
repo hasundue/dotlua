b71997ef0714e2fb9db5ebf157b174faaa5949bb
hasundue <hasundue@gmail.com>
2023-12-08 16:26:13 +0900

    chore(kanagawa): disable bold

--- lua/rc/kanagawa.lua
+++ lua/rc/kanagawa.lua
@@ -9,11 +9,16 @@ require('kanagawa').setup({
   commentStyle = { italic = false },
   functionStyle = {},
   keywordStyle = { italic = false },
-  statementStyle = { bold = true },
+  statementStyle = { bold = false },
   typeStyle = {},
   transparent = true,    -- do not set background color
   dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
   terminalColors = true, -- define vim.g.terminal_color_{0,17}
+  overrides = function()
+    return {
+      Boolean = { bold = false },
+    }
+  end,
 })
 
 vim.cmd('colorscheme kanagawa')
