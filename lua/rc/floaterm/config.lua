35a319ca909413dca1db23e894661c080330c2f2
hasundue <hasundue@gmail.com>
2024-02-29 21:28:51 +0900

    chore(floaterm): enlarge the window size

--- lua/rc/floaterm/config.lua
+++ lua/rc/floaterm/config.lua
@@ -1,6 +1,10 @@
 -- plugin-independent configuration
 require('rc.terminal')
 
+-- set the floaterm window size
+vim.g.floaterm_width = 0.8
+vim.g.floaterm_height = 0.8
+
 -- set highlights for floaterm windows
 vim.api.nvim_set_hl(0, 'Floaterm', { link = 'NormalFloat', force = true })
 vim.api.nvim_set_hl(0, 'FloatermBorder', { link = 'FloatBorder', force = true })
