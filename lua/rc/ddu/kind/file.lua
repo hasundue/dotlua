38393b077693e400ba123c152031ea4e3be03e5a
hasundue <hasundue@gmail.com>
2024-02-29 22:52:22 +0900

    feat(ddu): add keymap for `newFile` action

--- lua/rc/ddu/kind/file.lua
+++ lua/rc/ddu/kind/file.lua
@@ -3,6 +3,7 @@ local ddu = require('lib.ddu')
 ddu.map('file', {
   { '<C-s>', ddu.item_action('open', { command = 'split' }) },
   { '<C-v>', ddu.item_action('open', { command = 'vsplit' }) },
-  { '<C-r>', ddu.item_action('rename') },
-  { '<C-d>', ddu.item_action('delete') },
+  { '<M-r>', ddu.item_action('rename') },
+  { '<M-d>', ddu.item_action('delete') },
+  { '<M-n>', ddu.item_action('newFile') },
 })
