388e7656d95ab0d258cbb911cb8b4c2ae0f6a9a9
hasundue <hasundue@gmail.com>
2024-03-05 15:50:12 +0900

    chore(floaterm): specify fish as a shell

--- lua/rc/floaterm/init.lua
+++ lua/rc/floaterm/init.lua
@@ -1,6 +1,9 @@
 -- plugin-independent configuration
 require('rc.terminal')
 
+-- use fish
+vim.g.floaterm_shell = 'fish'
+
 -- set the floaterm window size
 vim.g.floaterm_width = 0.8
 vim.g.floaterm_height = 0.8
