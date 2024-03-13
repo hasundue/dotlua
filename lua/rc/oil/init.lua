583f02ef13c00f15f6cdb96f89d7310e2f1ba7b3
hasundue <hasundue@gmail.com>
2024-03-14 08:55:21 +0900

    chore(oil): use normal buffer for floating window

--- lua/rc/oil/init.lua
+++ lua/rc/oil/init.lua
@@ -1,1 +1,28 @@
-require("oil").setup()
+local winblend = vim.opt.winblend:get()
+
+require("oil").setup({
+  keymaps_help = {
+    border = "solid",
+  },
+  float = {
+    border = "solid",
+    win_options = {
+      winblend = winblend,
+    },
+  },
+  preview = {
+    border = "solid",
+    win_options = {
+      winblend = winblend,
+    },
+  },
+  progress = {
+    border = "solid",
+    win_options = {
+      winblend = winblend,
+    },
+  },
+  ssh = {
+    border = "solid",
+  },
+})
--- lua/rc/oil/keymap.lua
+++ lua/rc/oil/keymap.lua
@@ -1,3 +1,3 @@
 local oil = require("oil")
 
-vim.keymap.set("n", "<leader>o", oil.open_float)
+vim.keymap.set("n", "<leader>o", oil.open)
