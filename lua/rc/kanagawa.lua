2efc665e6db3ac3959cf2052c3bf15682ad25705
hasundue <hasundue@gmail.com>
2024-04-26 12:03:06 +0900

    feat(kanagawa): make background transparent

--- lua/rc/kanagawa.lua
+++ lua/rc/kanagawa.lua
@@ -10,26 +10,36 @@ require('kanagawa').setup({
   overrides = function(colors)
     local theme = colors.theme
     return {
+      -- Transparent background
+      NormalFloat = { bg = "none" },
+      FloatBorder = { bg = "none" },
+      FloatTitle = { bg = "none" },
+
+      -- Darker background
+      NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_dim },
+
       -- Dark completion menu
       Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
-      PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+      PmenuSel = { fg = "none", bg = theme.ui.bg_p2 },
       PmenuSbar = { bg = theme.ui.bg_m1 },
       PmenuThumb = { bg = theme.ui.bg_p2 },
+
       -- Borderless Telescope
       TelescopeTitle = { fg = theme.ui.special, bold = true },
       TelescopePromptNormal = { bg = theme.ui.bg_p1 },
       TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-      TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-      TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+      TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_dim },
+      TelescopeResultsBorder = { fg = theme.ui.bg_dim, bg = theme.ui.bg_dim },
       TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-      TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+      TelescopePreviewBorder = { fg = theme.ui.bg_dim, bg = theme.ui.bg_dim },
+
       -- Borderless Floaterm
-      Floaterm = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-      FloatermBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+      Floaterm = { fg = theme.ui.fg_dim, bg = theme.ui.bg_dim },
+      FloatermBorder = { fg = theme.ui.bg_dim, bg = theme.ui.bg_dim },
     }
   end,
   terminalColors = true, -- define vim.g.terminal_color_{0,17}
-  transparent = false,   -- set background color
+  transparent = true,    -- do not set background color
   undercurl = true,      -- enable undercurls
 })
 
