df932f27077f7d100b3e972513cf6d4e437b6a6d
hasundue <hasundue@gmail.com>
2024-05-10 16:25:38 +0900

    feat(incline): add devicon

--- lua/rc/incline.lua
+++ lua/rc/incline.lua
@@ -1,1 +1,19 @@
-require("incline").setup()
+local devicons = require("nvim-web-devicons")
+
+require("incline").setup({
+  window = {
+    margin = { horizontal = 0 },
+  },
+  render = function(props)
+    local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+    if filename == "" then
+      return nil
+    end
+    local ft_icon, ft_color = devicons.get_icon_color(filename)
+    local modified = vim.bo[props.buf].modified
+    return {
+      ft_icon and { ft_icon, " ", guifg = ft_color } or "",
+      { filename, gui = modified and "bold,italic" or "bold" },
+    }
+  end,
+})
