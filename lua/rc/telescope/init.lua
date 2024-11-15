a959c8ee667edf23953570ed9cef5ac20816e755
Shun Ueda <hasundue@gmail.com>
2024-11-15 09:49:18 +0000

    feat(telescope): include submodules in git_files

--- lua/rc/telescope/init.lua
+++ lua/rc/telescope/init.lua
@@ -2,6 +2,9 @@ require("telescope").setup({
   pickers = {
     find_files = {
       find_command = { "fd", "--type", "f", "--hidden", "--no-ignore", "--strip-cwd-prefix" }
-    }
+    },
+    git_files = {
+      recurse_submodules = true,
+    },
   }
 })
