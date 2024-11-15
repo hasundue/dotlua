7c8c997f6543403abafb7c49c10c2855a5a4d92f
Shun Ueda <hasundue@gmail.com>
2024-11-15 05:51:33 +0000

    fix: make sure system's nvim-treesitter loaded

--- init.lua
+++ init.lua
@@ -4,5 +4,8 @@ vim.loader.enable()
 -- base configuration, independent of plugins (keymaps, etc)
 require("rc")
 
+-- enable nvim-treesitter manuallly since it is not managed by dpp
+require("rc.treesitter")
+
 -- load plugins with dpp.vim
 require("rc.dpp")
