d6653d50d97e6f8b02f6cbffe9c5ad9ca012c38a
Shun Ueda <hasundue@gmail.com>
2024-11-28 08:23:32 +0000

    feat(dogfood): add packages and devShells

--- nix/dogfood.nix
+++ nix/dogfood.nix
@@ -3,13 +3,25 @@
 , neovim-flake
 , ...
 }:
-flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: {
-  apps = {
-    default = {
-      type = "app";
-      program = nixpkgs.lib.getExe (with neovim-flake.${system}; neovim {
-        modules = [ core lua nix ];
-      });
+flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+let
+  pkgs = nixpkgs.legacyPackages.${system};
+  lib = nixpkgs.lib;
+in
+rec {
+  packages = with neovim-flake.${system}; lib.mapAttrs
+    (name: modules: neovim { inherit modules; })
+    {
+      default = [ core lua nix ];
+      deno = [ core deno ];
     };
-  };
+  devShells = lib.mapAttrs
+    (name: package: pkgs.mkShell {
+      inherit name;
+      packages = [ package ];
+      shellHook = ''
+        alias nv=nvim
+      '';
+    })
+    packages;
 })
