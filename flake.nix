4da0492124033a67a8a9195593a31452f03bcf35
Shun Ueda <hasundue@gmail.com>
2024-12-06 04:43:15 +0000

    feat: create overlays

--- flake.nix
+++ flake.nix
@@ -34,5 +34,12 @@
             attrs // { all = lib.attrValues attrs; };
         }
       ) //
+    {
+      overlays = {
+        default = final: prev: {
+          mkNeovim = self.${final.system};
+        };
+      };
+    } //
     (import ./nix/dogfood.nix (inputs // { nvim-flake = self; }));
 }
